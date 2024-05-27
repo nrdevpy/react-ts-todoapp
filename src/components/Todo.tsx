2720c13699333e29f765ba32705b8da5e672261e
NrDev <nelson_rojas_janda@hotmail.com>
2024-05-27 09:35:42 +0200

    Refacto Todo component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -6,13 +6,17 @@ interface Props extends TodoType {
 }
 
 export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleComplete }) => {
+    const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        onToggleComplete({ id, completed: event.target.checked })
+    }
+
     return (
         <div className='view'>
             <input
                 className="toggle"
                 type="checkbox"
                 checked={completed}
-                onChange={event => onToggleComplete({ id, completed: event.target.checked })}
+                onChange={event => handleChangeCheckbox(event)}
             />
             <label>{title}</label>
             <button className="destroy" onClick={() => onRemoveTodo({ id })}></button>
